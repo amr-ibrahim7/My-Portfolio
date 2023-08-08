db3e4a102b6e6c039ab0ef05b237475ad974d95d
amr-ibrahim7 <amrnew100@gmail.com>
2023-08-08 18:56:11 +0300

    editing CV link

--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
 // Import Image
-import Image from '../assets/img/about.jpg';
+import Image from "../assets/img/about.jpg";
 const About = () => {
   return (
     <section
-      id='about'
-      className='section
+      id="about"
+      className="section
     bg-secondary
-    '
+    "
     >
-      <div className='container mx-auto'>
-        <div className='flex flex-col xl:flex-row gap-24'>
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-24">
           <img
-            className='object-cover h-full w-[566px] md:mx-auto lg:mx-0 rounded-2xl'
+            className="object-cover h-full w-[566px] md:mx-auto lg:mx-0 rounded-2xl"
             src={Image}
-            alt=''
+            alt=""
           />
-          <div className='flex flex-col items-center text-center lg:items-start lg:text-left'>
-            <div className='flex flex-col'>
-              <h2 className='text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3 before:content-about relative before:absolute before :opacity-40 before:-top-[2rem] before:hidden before:lg:block'>
+          <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
+            <div className="flex flex-col">
+              <h2 className="text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3 before:content-about relative before:absolute before :opacity-40 before:-top-[2rem] before:hidden before:lg:block">
                 Amr Ibrahim
               </h2>
-              <p className='mb-4 text-accent'>Freelance Web Devloper</p>
-              <hr className='mb-8 opacity-5' />
-              <p className='mb-8'>
+              <p className="mb-4 text-accent">Freelance Web Devloper</p>
+              <hr className="mb-8 opacity-5" />
+              <p className="mb-8">
                 Hello there, My Name is Amr Ibrahim, and I am a dedicated
                 front-end developer with a passion for creating immersive and
                 user-centric web experiences. With 1 year of experience in the
@@ -34,14 +34,14 @@ const About = () => {
                 skills, to make the best use of all what I have learned.
               </p>
             </div>
-            <button className='btn btn-md bg-accent hover:bg-accent-hover md:btn-lg transition-all'>
+            <button className="btn btn-md bg-accent hover:bg-accent-hover md:btn-lg transition-all">
               <a
-                rel='noreferrer'
-                target='__self'
-                href='https://drive.google.com/uc?export=download&id=1RAEpaRsnXvf2dzVv7TZSn0LGXI3m8Kjp'
+                rel="noreferrer"
+                target="__self"
+                href="https://drive.google.com/uc?export=download&id=1XlFyoWYGB2oLKfWi1sHGMLQ58ZUzOcRr"
               >
                 Download My Resume
-              </a>{' '}
+              </a>{" "}
             </button>
           </div>
         </div>
